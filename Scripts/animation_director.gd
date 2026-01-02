2eddb2c77066e0f61c6e59e73bdd3a73037f5491
BlaerZhang <zhangyueyang0022@126.com>
2026-01-02 11:32:50 -0500

    UPDATE: animation director set to global and autoload

--- Scripts/animation_director.gd
+++ Scripts/animation_director.gd
@@ -1,12 +1,11 @@
 extends Node
-class_name AnimationDirector
+
+var global_playback_speed: float = 1.0
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
