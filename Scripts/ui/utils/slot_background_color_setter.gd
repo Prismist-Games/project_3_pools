46158c2fa5f6d47f2c8d70408f05d761bef0e681
BlaerZhang <zhangyueyang0022@126.com>
2026-01-06 12:05:16 -0500

    NEW: Finished Interface Mockup

--- Scripts/ui/utils/slot_background_color_setter.gd
+++ Scripts/ui/utils/slot_background_color_setter.gd
@@ -4,12 +4,12 @@ extends Node2D
 @onready var background_wall: Sprite2D = $Slot_background_wall
 @onready var background_side: Sprite2D = $Slot_background_side
 @onready var background_floor: Sprite2D = $Slot_background_floor
-@export var color: Color = Color.WHITE:
-	set(value):
-		color = value
-		background_wall.modulate = color
-		background_side.modulate = color
-		background_floor.modulate = color
+@export var color: Color = Color.WHITE
+	#set(value):
+		#color = value
+		#background_wall.modulate = color
+		#background_side.modulate = color
+		#background_floor.modulate = color
 
 
 func _ready() -> void:
